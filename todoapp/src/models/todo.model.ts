f77ec3ccded0ff3b7c1d3c4b6373eb9511c1bf2d
walid885 <bettaieb88walid@gmail.com>
2023-02-23 17:26:13 +0100

    creation de modele de BD

---
+++ todoapp/src/models/todo.model.ts
@@ -0,0 +1,39 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+  })
+  descrp?: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  IsDone: boolean;
+
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
+
+export interface TodoRelations {
+  // describe navigational properties here
+}
+
+export type TodoWithRelations = Todo & TodoRelations;
